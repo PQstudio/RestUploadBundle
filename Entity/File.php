896f1f5f16e8c5c174099a96742b01488fb5c23b
Grzegorz Graczyk <gregory90@gmail.com>
2014-05-20 15:10:49 +0200

    Add Timestapable to File entity

--- Entity/File.php
+++ Entity/File.php
@@ -3,6 +3,8 @@
 namespace PQstudio\RestUploadBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
+use Gedmo\Mapping\Annotation as Gedmo;
 
 /**
  * @ORM\Entity
@@ -10,6 +12,7 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class File
 {
+    use TimestampableEntity;
 
     /**
      * @ORM\Id
